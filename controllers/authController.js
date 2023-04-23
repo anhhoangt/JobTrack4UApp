55c72f00786d985a5a8c57873455d832eeb2bc9f
anhhoang <anhhoang0319@gmail.com>
2023-04-23 16:02:38 -0700

    adding confirmEmail api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,45 @@
-import User from '../models/User.js';
-import { StatusCodes } from 'http-status-codes';
-import { BadRequestError, UnAuthenticatedError } from '../errors/index.js';
-import attachCookie from '../utils/attachCookie.js';
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+import attachCookie from "../utils/attachCookie.js";
+import sendgrid from "@sendgrid/mail";
+
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    throw new BadRequestError('please provide all values');
+    throw new BadRequestError("please provide all values");
   }
   const userAlreadyExists = await User.findOne({ email });
   if (userAlreadyExists) {
-    throw new BadRequestError('Email already in use');
+    throw new BadRequestError("Email already in use");
   }
   const user = await User.create({ name, email, password });
 
   const token = user.createJWT();
   attachCookie({ res, token });
+  sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const msg = {
+    to: email,
+    from: process.env.SENDGRID_SENDER,
+    subject: "Welcome to Jobify, thanks for choosing us",
+    text: "your account need to be verified",
+    html: `
+    <p> This is email confirm your account on our website has been activated. Hope you have a wonderful experience with us</p>
+
+`,
+  };
+  sendgrid
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode);
+      console.log(response[0].headers);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   res.status(StatusCodes.CREATED).json({
     user: {
       email: user.email,
@@ -30,16 +54,16 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    throw new BadRequestError('Please provide all values');
+    throw new BadRequestError("Please provide all values");
   }
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email }).select("+password");
   if (!user) {
-    throw new UnAuthenticatedError('Invalid Credentials');
+    throw new UnAuthenticatedError("Invalid Credentials");
   }
 
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
-    throw new UnAuthenticatedError('Invalid Credentials');
+    throw new UnAuthenticatedError("Invalid Credentials");
   }
   const token = user.createJWT();
   attachCookie({ res, token });
@@ -50,7 +74,7 @@ const login = async (req, res) => {
 const updateUser = async (req, res) => {
   const { email, name, lastName, location } = req.body;
   if (!email || !name || !lastName || !location) {
-    throw new BadRequestError('Please provide all values');
+    throw new BadRequestError("Please provide all values");
   }
   const user = await User.findOne({ _id: req.user.userId });
 
@@ -73,11 +97,11 @@ const getCurrentUser = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-  res.cookie('token', 'logout', {
+  res.cookie("token", "logout", {
     httpOnly: true,
     expires: new Date(Date.now() + 1000),
   });
-  res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
+  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
 };
 
 export { register, login, updateUser, getCurrentUser, logout };
