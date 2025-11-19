b1a4925919f3d5548be4fd822c571928c7fbbe7f
Anh Hoang <anhthehoang@meta.com>
2025-11-19 15:57:44 -0800

    small fix with cloudinary

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -31,9 +31,9 @@ const cloudinary = cloudinaryModule.v2;
 
 // Configure Cloudinary with environment variables
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 /**
